dc1fd157df64d5c52dbb91ced3655e3dd3dfc9d4
icoder-new <priler05@gmail.com>
2023-06-14 03:24:26 +0500

    added err when user id not found

--- utils/errors.go
+++ utils/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrInvalidSigningKey   = errors.New("invalid signing method")
 	ErrInvalidTypeOfClaims = errors.New("token claims are not type of `*tokenClaims`")
+	ErrUserIdNotFound      = errors.New("user id not found")
 )
