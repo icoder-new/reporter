a74107a80dcfa4a59f2142aed42f1bdddea21eea
icoder-new <priler05@gmail.com>
2023-06-15 17:55:45 +0500

    ADDED: new errors

--- utils/errors.go
+++ utils/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrInvalidSigningKey   = errors.New("invalid signing method")
 	ErrInvalidTypeOfClaims = errors.New("token claims are not type of `*tokenClaims`")
 	ErrUserIdNotFound      = errors.New("user id not found")
+	ErrTooMuchBalance      = errors.New("too much balance, your limit is under 5000")
+	ErrInvalidAccountName  = errors.New("invalid account name")
 )
