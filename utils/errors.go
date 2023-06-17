74b5c2a158a4fe711e2ed52f748388509a33b7f2
icoder-new <priler05@gmail.com>
2023-06-18 00:23:55 +0500

    NEW: added new errors

--- utils/errors.go
+++ utils/errors.go
@@ -3,9 +3,10 @@ package utils
 import "errors"
 
 var (
-	ErrInvalidSigningKey   = errors.New("invalid signing method")
-	ErrInvalidTypeOfClaims = errors.New("token claims are not type of `*tokenClaims`")
-	ErrUserIdNotFound      = errors.New("user id not found")
-	ErrTooMuchBalance      = errors.New("too much balance, your limit is under 5000")
-	ErrInvalidAccountName  = errors.New("invalid account name")
+	ErrInvalidSigningKey    = errors.New("invalid signing method")
+	ErrInvalidTypeOfClaims  = errors.New("token claims are not type of `*tokenClaims`")
+	ErrUserIdNotFound       = errors.New("user id not found")
+	ErrTooMuchBalance       = errors.New("too much balance, your limit is under 5000")
+	ErrInvalidAccountName   = errors.New("invalid account name")
+	ErrUnsupportedMediaType = errors.New("unsupported media type, type must be 'jpg' or 'jpeg'")
 )
