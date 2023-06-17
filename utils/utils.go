185b51032ea17741fb41058e62091f98b2a162ac
icoder-new <priler05@gmail.com>
2023-06-18 00:25:00 +0500

    NEW: generate filename with directory [TODO]

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,13 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func CheckField(field string) bool {
 	if field == "" || field == " " || len(field) > 50 {
@@ -33,4 +40,26 @@ func GeneratePassword(password string) (string, error) {
 
 func CheckPassword(hashPassword, password string) bool {
 	return IsChangeablePassword(hashPassword, password) == nil
+}
+
+func GenFilenameWithDir(filename string) (string, error) {
+	validExtensions := []string{".jpg", ".jpeg", ".png"}
+
+	fileExt := strings.ToLower(filepath.Ext(filename))
+
+	for _, ext := range validExtensions {
+		if ext == fileExt {
+			timeSign := fmt.Sprintf("%d", time.Now().UnixNano())
+			filePath := fmt.Sprintf("%s_%s", timeSign, filename)
+			filePath = strings.Replace(filePath, " ", "", 111)
+
+			return filePath, nil
+		}
+	}
+
+	return "", ErrUnsupportedMediaType
+}
+
+func GetContentType(filePath string) string {
+	return fmt.Sprintf("image/%s", strings.Replace(filepath.Ext(filePath), ".", "", 111))
 }
